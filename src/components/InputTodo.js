13f670667aa7f668be17cceca0165974d0d46bab
Lucas Comamala <lucascomamala@gmail.com>
2022-09-12 17:32:28 -0600

    Add onChange handler to input

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,10 +1,19 @@
 import React from "react"
 
 class InputTodo extends React.Component {
+
+  state = {
+    title: ""
+  };
+
+  onChange = e => {
+    console.log("hello");
+  };
+  
   render() {
     return (
       <form>
-        <input type="text" placeholder="Add Todo..." />
+        <input type="text" placeholder="Add todo..." value={this.state.title} onChange={this.onChange} />
         <button>Submit</button>
       </form>
     )
