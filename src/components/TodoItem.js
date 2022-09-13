872820cc617416f84f7f77de4abf5857997ce625
Lucas Comamala <lucascomamala@gmail.com>
2022-09-12 23:19:36 -0600

    Exit edit mode

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -13,6 +13,12 @@ class TodoItem extends React.Component {
     })
   }
 
+  handleUpdatedDone = event => {
+    if (event.key === "Enter") {
+      this.setState({ editing: false })
+    }
+  }
+
   render() {
     const completedStyle = {
       fontStyle: "italic",
@@ -52,6 +58,7 @@ class TodoItem extends React.Component {
           onChange={e => {
             this.props.setUpdate(e.target.value, id)
           }}
+          onKeyDown={this.handleUpdatedDone}
         />
       </li >
     )
