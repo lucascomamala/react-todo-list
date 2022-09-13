aa7794ef3d87ccfbefd3e0f534a4f5782e72178d
Lucas Comamala <lucascomamala@gmail.com>
2022-09-13 14:22:11 -0600

    Change to root render

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import { BrowserRouter } from "react-router-dom"
 import TodoContainer from "./components/TodoContainer"
 import "./app.css"
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <TodoContainer />
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById("root")
-)
+);
